ff904d8d1fbe8d271bc5ae71b257c024c4081cac
user <user@NEC-VK24LXH>
2025-10-07 19:19:46 +0900

    テキストchapter1コーディング練習(牧)

---
+++ ch1/src/ch1/Main1_1.java
@@ -0,0 +1,13 @@
+package ch1;
+
+public class Main1_1 {
+
+	public static void main(String[] args) {
+		// コード1-1　ソースコードの基本構造
+		System.out.println("RPG:スッキリ魔王討伐");
+		System.out.println("Ver.0.1 by 湊");
+		System.out.println("＜ただいま鋭意学習・制作中＞");
+		System.out.println("プログラムを終了します");
+	}
+
+}
---
+++ ch1/src/ch1/Main1_2.java
@@ -0,0 +1,12 @@
+package ch1;
+
+public class Main1_2 {
+
+	public static void main(String[] args) {
+		// コード1-2  変数宣言の文
+		int age;
+		age = 30;
+		System.out.println(age);
+	}
+
+}
